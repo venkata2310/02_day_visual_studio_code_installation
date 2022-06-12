30f908604796b9fbb50790852501d94e4afa3e72
VENKATA RAGURAM KN <107139560+venkata2310@users.noreply.github.com>
2022-06-12 23:35:48 +0530

    Add files via upload

---
+++ 2nd class.py
@@ -0,0 +1,54 @@
+#x= 5+2j 
+#print(type(x))
+
+#w = {}
+#print(type(w))
+
+#a= {1,2}
+#print(type(a))
+
+#p= {1:"hello", 2:'hi', 3:'yo!'}
+#print(type(p))
+#print(tuple(p))
+#print(p[1])
+
+#c= (b'hello')
+#print(type(c))
+#d= bytearray(c)
+#print(type(d))
+
+#n= (10,2,3,5,'hello', 8)
+#v= enumerate(n,65)
+#print(list(v))
+
+#print('please enter your name below!!')
+#n= input()
+#print('You are welcome', n)
+#print('we will look forward for your approval dear', n)
+#print('please enter your age below')
+#x= input()
+#print('you are elgible for the above sclorship dear', n)
+#print( '**congratulations**')
+
+#x= 125
+#print(int(x))
+#print(float(x))
+#print(str(x))
+#print(type(x))
+#print(bool(x))
+
+#for i in range (1,10):
+    #print(i)
+
+##for getting only odd values:
+#for i in range(1,10,2):
+    #print(i)
+
+## for getting even values:
+#for i in range(2,10,2):
+    #print(i)
+
+#x= 10+2j
+#print(type(x))
+
+
